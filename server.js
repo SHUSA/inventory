3312c8ae36cc87e10148699df51d6314778c5a70
rukaroa <rukaroa@gmail.com>
2018-04-26 10:06:25 -0500

    server refactored

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const { sequelize } = require('./models')
+
+const config = require('./config/config')
 
 // create the express app
 const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+// require('./routes')(app)
 
 // create middleware to handle the serving the app
 app.use("/", serveStatic(path.join(__dirname, '/dist')))
@@ -13,9 +23,7 @@ app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
 })
 
-// Create default port to serve the app on
-const port = process.env.PORT || 5000
-app.listen(port)
-
-// Log to feedback that this is actually running
-console.log('Server started on port ' + port)
+sequelize.sync({ force: false }).then(() => {
+  app.listen(config.port)
+  console.log(`Server started on port ${config.port}`)
+})
