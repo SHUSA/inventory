4eee9ffe650dc09b7d7fa9ed54880477e7f0ceb8
rukaroa <rukaroa@gmail.com>
2018-04-26 12:36:16 -0500

    header started

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-toolbar  fixed dark>
+    <v-toolbar-title>
+      Title
+    </v-toolbar-title>
+
+    <v-toolbar-items>
+      <v-btn>
+        Test
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
