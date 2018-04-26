d7b9cc71189011a851bf01f6412a2e5dac9a8a6d
rukaroa <rukaroa@gmail.com>
2018-04-26 11:21:15 -0500

    vuetify imported

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Veutify from 'vuetify'
+// import { sync } from 'vuex-router-sync'
+import 'vuetify/dist/vuetify.min.css'
+// import store from '@/store/store'
 
 Vue.config.productionTip = false
+Vue.use(Veutify)
+
+// sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // store,
   components: { App },
   template: '<App/>'
 })
