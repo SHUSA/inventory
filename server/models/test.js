16b3b6bce62c7f5e73e1c94d7726fb38eeb0c0e7
rukaroa <rukaroa@gmail.com>
2018-05-02 11:33:16 -0500

    just testing string and number

--- server/models/test.js
+++ server/models/test.js
@@ -1,8 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Test = sequelize.define('Test', {
     number: DataTypes.FLOAT,
-    text: DataTypes.STRING,
-    json: DataTypes.JSON
+    text: DataTypes.STRING
   })
 
   return Test
