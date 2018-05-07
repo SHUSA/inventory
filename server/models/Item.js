d7559bd4b0099a2b8d7c69141ebb11c76a02b105
rukaroa <rukaroa@gmail.com>
2018-05-07 11:32:12 -0500

    item model started

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,5 +1,28 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User')
+  const Item = sequelize.define('Item', {
+    name: DataTypes.STRING,
+    catalogNumber: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    itemDescription: DataTypes.TEXT,
+    reactionsPerItem: DataTypes.INTEGER,
+    // create quantity history model instead
+    quantityOnHand: DataTypes.INTEGER,
+    weeksOfSafetyStock: DataTypes.TINYINT,
+    leadTimeDays: DataTypes.TINYINT,
+    weeksOfReorder: DataTypes.TINYINT,
+    // create comment history model instead
+    comments: {
+      type: DataTypes.TEXT,
+      autoIncrement: true,
+      updatedAt: DataTypes.DATE
+    },
+    updatedAt: DataTypes.DATE,
+    image: DataTypes.BLOB
+  })
 
-  return User
+  // Item.belongsToMany
+
+  return Item
 }
