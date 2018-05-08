b91ad9d8157a8848c0e89e4a405ad8678efc90fc
rukaroa <rukaroa@gmail.com>
2018-05-08 11:52:11 -0500

    import nav drawer

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,20 @@
 <template>
-  <p>Test</p>
+  <v-layout>
+    <v-flex xs2>
+      <navigation></navigation>
+    </v-flex>
+    <v-flex xs10>
+      test
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import Navigation from './Navigation'
 export default {
-  
+  components: {
+    Navigation
+  }
 }
 </script>
 
