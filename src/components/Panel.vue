357be2893ed55c03c1d8a19e5f38fc12a502223d
rukaroa <rukaroa@gmail.com>
2018-05-09 09:26:33 -0500

    added padding and slot section

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,9 +1,8 @@
 <template>
   <v-layout>
-    <v-flex>
-      <v-card>
-        <v-card-text>
-          <p>
+    <v-flex xs12>
+      <v-card class="pa-3">
+          <slot>
             Lorem ipsum dolor sit amet, copiosae urbanitas nam ut, labores albucius has ea. Ius animal legimus utroque ea. Ad augue ubique pericula sed, bonorum democritum argumentum mel at. At labores voluptatum has, ei justo regione omnesque vix, et purto nullam duo. Mea no commodo voluptaria consequuntur, ex vim ornatus accusamus constituam.
 
 Ei congue aliquando mel, in expetendis liberavisse sit. Eos decore fabulas tibique ea. Eu iracundia similique contentiones vel. Eum docendi lobortis cu, an sed scripta tritani eleifend. Eum te ludus iriure fuisset, paulo tantas doming ea per. Pri audire efficiantur an.
@@ -13,8 +12,7 @@ Ne case aeque dicam vim, paulo doctus salutandi at qui. Has vero voluptua ut, fa
 Elitr officiis maluisset cum ad. At duo utinam diceret, cu vim probo cotidieque delicatissimi, te purto propriae delectus eos. Per agam admodum sadipscing et. Ne mel maiestatis reprimique. Vis in utamur commodo equidem, paulo eligendi eu pri. Te eos tractatos salutandi signiferumque, qui ei autem erroribus, vel aperiri iracundia id.
 
 Ipsum fabulas incorrupte in eam, per et eirmod impetus. Nec ea zril mandamus scriptorem. An sonet legimus sit, vim et autem mnesarchum, ad quas probatus gloriatur nec. Ea quo malis mundi. Per inani aliquid disputando ne, id qui omittam repudiandae, nam ex ubique senserit hendrerit.
-          </p>
-        </v-card-text>
+          </slot>
       </v-card>
     </v-flex>
   </v-layout>
