e6cbc21a99f1228d843409de5866ef24ab5f875d
rukaroa <rukaroa@gmail.com>
2018-05-15 08:05:42 -0500

    placeholder login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,25 @@
 <template>
-  
+  <v-layout>
+    <v-flex xs2></v-flex>
+    <v-flex xs8>
+      <v-card>
+        <v-card-title>
+          <h3 class="headline">Who are you?</h3>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat>User</v-btn>
+          <v-btn flat>Admin</v-btn>
+          <v-btn flat>Demo</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    <v-flex xs2></v-flex>
+  </v-layout>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
