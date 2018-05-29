cae6b9640aff41398c5f8e3e74f2537f9265f0c1
rukaroa <rukaroa@gmail.com>
2018-05-29 12:04:53 -0500

    auth controller created

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -11,9 +11,65 @@ function jwtSignUser (user) {
 
 module.exports = {
   async register (req, res) {
+    const user = req.body
+    const userData = {
+      email: user.email,
+      password: user.password,
+      initials: user.initials,
+      username: user.username
+    }
+    const newUser = new User(userData)
+    const userJson = newUser.toJSON()
 
+    try {
+      await newUser.save((err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send({
+            user: userJson,
+            token: jwtSignUser(userJson)
+          })
+        }
+      })
+    } catch (error) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
   },
-  async login (req, res) {
 
+  async login (req, res) {
+    const {name, password} = req.body
+    // name field for general login
+    try {
+      await User.find({
+        $or: [
+          {email: name},
+          {usename: name}
+        ]}, (err, user) => {
+        if (err) {
+          console.log(err)
+        } else {
+          const isPasswordValid = user.comparePassword(password)
+
+          if (!user || !isPasswordValid) {
+            return res.status(403).send({
+              error: 'Login information is incorrect'
+            })
+          }
+
+          const userJson = user.toJSON()
+          res.send({
+            user: userJson,
+            token: jwtSignUser(userJson)
+          })
+        }
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
+      })
+    }
   }
 }
