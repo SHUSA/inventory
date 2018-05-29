511a19e8a0c3138b2c9ec91d08c860568fea4c60
rukaroa <rukaroa@gmail.com>
2018-05-29 08:26:20 -0500

    assay controller created

--- server/controllers/AssaysController.js
+++ server/controllers/AssaysController.js
@@ -1,30 +1,96 @@
 const Assay = require('mongoose').model('Assay')
+// const Issue = require('../models/Issues')
 
 module.exports = {
   async index (req, res) {
-    Assay.find({}).sort({name: -1}, (err, doc) => {
-      if (err) {
-        console.log(err)
-        res.status(500).send({
-          error: 'An error occured fetching assays'
-        })
-      } else {
-        res.send(doc)
-      }
-    })
+    try {
+      await Assay.find({}).sort({name: -1}, (err, doc) => {
+        if (err) {
+          console.log(err)
+          // planned issue logger
+          // Issue.issue(err, 'assay all')
+        } else {
+          res.send(doc)
+        }
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error occured fetching assays'
+      })
+    }
   },
 
   async show (req, res) {
-    const assay = req.body._id
-    
-    Assay.find({})
+    const assay = req.body
+
+    try {
+      await Assay.find({_id: assay.id}, (err, doc) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send(doc)
+        }
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error occured fetching assay'
+      })
+    }
   },
 
   async post (req, res) {
+    const assay = req.body
+    const assayData = {
+      name: assay.name,
+      weeklyVolume: assay.weeklyVolume,
+      weeklyRuns: assay.weeklyRuns,
+      controlsPerRun: assay.controlsPerRun,
+      maxBatchSize: assay.maxBatchSize,
+      sampleReplicates: assay.sampleReplicates,
+      active: true
+    }
+    const newAssay = new Assay(assayData)
 
+    try {
+      await newAssay.save((err) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send('Saved assay')
+          // res.redirect(where?)
+        }
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error occured saving assay'
+      })
+    }
   },
 
   async put (req, res) {
+    const assay = req.body
+    const assayData = {
+      name: assay.name,
+      weeklyVolume: assay.weeklyVolume,
+      weeklyRuns: assay.weeklyRuns,
+      controlsPerRun: assay.controlsPerRun,
+      maxBatchSize: assay.maxBatchSize,
+      sampleReplicates: assay.sampleReplicates,
+      active: true
+    }
 
+    try {
+      await Assay.update({_id: req.params.id}, assayData, (err, doc) => {
+        if (err) {
+          console.log(err)
+        } else {
+          res.send(doc)
+        }
+      })
+    } catch (error) {
+      res.status(500).send({
+        error: 'An error occured updating assay'
+      })
+    }
   }
 }
