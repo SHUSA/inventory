913aa599930b7a94dea43acc276cc210b8497bce
rukaroa <rukaroa@gmail.com>
2018-06-01 12:36:16 -0500

    item list does not need to be stored in assay or vendor

--- server/controllers/ItemsController.js
+++ server/controllers/ItemsController.js
@@ -1,7 +1,5 @@
 const Item = require('mongoose').model('Item')
-const Vendor = require('mongoose').model('Vendor')
 const Order = require('mongoose').model('Order')
-const Assay = require('mongoose').model('Assay')
 // should probably search by assay or vendor then display items
 module.exports = {
   async index (req, res) {
@@ -46,34 +44,16 @@ module.exports = {
     }
     const newItem = new Item(itemData)
 
+    if (newItem.reactionsPerItem !== 0) {
+      // calculate safety stock, reorder point, reorder quantity
+    }
+
     try {
       await newItem.save((err, doc) => {
         if (err) {
           console.log(err)
           res.send(err.message)
         } else {
-          Assay.findOneAndUpdate({name: item.assay},
-            {$addToSet: {items: doc._id}}, {new: true}, (err, newdoc) => {
-              if (err) {
-                console.log(err)
-                res.send(err.message)
-              } else {
-                // specify assay, do a console.log on newdoc
-                console.log('item inserted into assay')
-              }
-            })
-          Vendor.findOneAndUpdate({
-            $or: [{name: item.vendor}, {shortName: item.vendor}]
-          },
-          { $addToSet: { items: doc._id } }, { new: true }, (err, newdoc) => {
-            if (err) {
-              console.log(err)
-              res.send(err.message)
-            } else {
-              // specify assay, do a console.log on newdoc
-              console.log('item inserted into vendor')
-            }
-          })
           Order.find().sort({_id: -1}).limit(1)
             .updateOne({ $addToSet: { items: doc._id } },
               { new: true }, (err, newdoc) => {
@@ -110,28 +90,6 @@ module.exports = {
           console.log(err)
           res.send(err.message)
         } else {
-          Assay.findOneAndUpdate({ name: item.assay },
-            { $addToSet: { items: tempId } }, { new: true }, (err, newdoc) => {
-              if (err) {
-                console.log(err)
-                res.send(err.message)
-              } else {
-                // specify assay, do a console.log on newdoc
-                console.log('item inserted into assay')
-              }
-            })
-          Vendor.findOneAndUpdate({
-            $or: [{name: item.vendor}, {shortName: item.vendor}]
-          },
-          { $addToSet: { items: tempId } }, { new: true }, (err, newdoc) => {
-            if (err) {
-              console.log(err)
-              res.send(err.message)
-            } else {
-              // specify assay, do a console.log on newdoc
-              console.log('item inserted into vendor')
-            }
-          })
           Order.find().sort({ _id: -1 }).limit(1)
             .updateOne({ $addToSet: { items: tempId } },
               { new: true }, (err, newdoc) => {
--- server/models/Vendor.js
+++ server/models/Vendor.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema
 const VendorSchema = new Schema({
   name: {type: String, unique: true, required: true},
   shortName: {type: String},
-  items: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Item'
-  }],
   active: {type: Boolean, default: true}
 })
 
