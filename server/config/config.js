3fcb38f21c9665617d7d5942aa3de63781698f98
rukaroa <rukaroa@gmail.com>
2018-06-21 08:58:40 -0500

    more connection tests

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 const config = require('./index.json')
-
+console.log('server')
+console.log(process.env)
 module.exports = {
   port: process.env.PORT || 8081,
   db: process.env.MONGODB_URI || config.dbUri,
