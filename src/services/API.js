fb99645c76709c3872ac198fd5d0bcbc0182a16b
rukaroa <rukaroa@gmail.com>
2018-06-21 07:26:30 -0500

    reverting baseurl; to troubleshoot

--- src/services/API.js
+++ src/services/API.js
@@ -1,10 +1,7 @@
 import axios from 'axios'
 
 export default () => {
-  console.log('base Url')
-  console.log(process.env.baseUrl)
-  console.log(process.env.baseURL)
   return axios.create({
-    baseURL: process.env.baseUrl || 'http://localhost:8081/'
+    baseURL: process.env.baseURL || 'http://localhost:8081/'
   })
 }
