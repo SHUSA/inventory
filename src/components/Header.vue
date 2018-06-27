9e9b168f79b87ed1439ae68a47ede4684b70a52f
rukaroa <rukaroa@gmail.com>
2018-06-27 07:45:44 -0500

    alpha build 3

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar app clipped-left flat dark>
     <v-toolbar-title>
-      {{title}} v.a-build-2
+      {{title}} v.a-build-3
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
