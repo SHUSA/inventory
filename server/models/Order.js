7916317fc7193a1b3ca498e2bfbc06af24eba2e0
rukaroa <rukaroa@gmail.com>
2018-06-29 12:03:50 -0500

    sequelized Order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,10 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
-  const Order = sequelize.define('Order', {})
-
-  Order.associate = function (models) {
-  }
-
-  // possibly use to make order history retrieval easier?
+  const Order = sequelize.define('Order', {
+    createDate: {
+      type: DataTypes.DATE,
+      defaultValue: Date.now()
+    },
+    completeDate: DataTypes.DATE,
+    completed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  })
 
   return Order
 }
