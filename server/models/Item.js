06b1d4600b1d72720f85945ac9530bec19ed477d
rukaroa <rukaroa@gmail.com>
2018-06-29 12:03:39 -0500

    sequelized Item model

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,25 +1,62 @@
 module.exports = (sequelize, DataTypes) => {
   const Item = sequelize.define('Item', {
-    name: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      required: true
+    },
     catalogNumber: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      required: true
     },
     itemDescription: DataTypes.TEXT,
-    reactionsPerItem: DataTypes.INTEGER,
-    weeksOfSafetyStock: DataTypes.TINYINT,
-    leadTimeDays: DataTypes.TINYINT,
-    weeksOfReorder: DataTypes.TINYINT,
-    updatedAt: DataTypes.DATE,
+    reactionsPerItem: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    currentStock: {
+      type: DataTypes.REAL,
+      defaultValue: 0
+    },
+    weeksOfSafetyStock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    safetyStock: {
+      type: DataTypes.REAL,
+      defaultValue: 0
+    },
+    leadTimeDays: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    weeksOfReorder: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    reorderPoint: {
+      type: DataTypes.REAL,
+      defaultValue: 0
+    },
+    reorderQuantity: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    comment: DataTypes.STRING,
+    lastUpdate: {
+      type: DataTypes.DATE,
+      defaultValue: Date.now()
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    },
     image: DataTypes.BLOB
   })
 
   Item.associate = function (models) {
     Item.belongsTo(models.Assay)
     Item.belongsTo(models.Vendor)
-    // Item.belongsToMany(models.Order, {
-    //   through: OrderHistory
-    // })
   }
 
   return Item
