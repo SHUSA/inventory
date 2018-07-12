7234324ca54531fd0764bf159f9eafbf6d7bd33a
rukaroa <rukaroa@gmail.com>
2018-07-12 12:32:36 -0500

    updated to include NEWDB env for new dbs

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.get('*', function (req, res) {
     res.sendFile(__dirname + '/dist/index.html')
 })
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: process.env.NEWDB || false }).then(() => {
   app.listen(config.port)
+  if (process.env.NEWDB) console.log('Tables have been formatted')
   console.log(`Server started on port ${config.port}`)
  })
 
