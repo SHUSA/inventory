8b9a0ee3d036e22112e5533138c963a9497aca81
rukaroa <rukaroa@gmail.com>
2018-07-12 07:15:26 -0500

    minor build update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar app clipped-left flat dark>
     <v-toolbar-title>
-      {{title}} v.a-build-6
+      {{title}} v.a-build-7
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
