c4f059b07412608092a90f5c66706ae281b89ba9
rukaroa <rukaroa@gmail.com>
2018-07-16 12:38:48 -0500

    setting up info tab; working on transitions

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,14 +2,26 @@
   <div>
     <v-tabs
       dark
+      v-model="active"
     >
-      <v-tab>Main</v-tab>
-      <v-tab>Coming Soon&#8482;</v-tab>
+      <v-tab href="#tab-1">Main</v-tab>
+      <v-tab href="#tab-2">Coming Soon&#8482;</v-tab>
+      <v-tab-item
+        v-for="n in 2"
+        :id="'tab-' + n"
+        :key="n"
+      >
+          <template v-if="active == 'tab-1'">
+            <order v-if="search === 'order'" :order="selection" :vendors="vendors"/>
+            <template v-else>
+              <inventory :items="selection" :assays="assays" :vendors="vendors" :orders="orders"/>
+            </template>
+          </template>
+          <template v-else>
+            {{JSON.stringify(infoTab)}}
+          </template>
+      </v-tab-item>
     </v-tabs>
-    <order v-if="search === 'order'" :order="selection" :vendors="vendors"/>
-    <template v-else>
-      <inventory :items="selection" :assays="assays" :vendors="vendors" :orders="orders"/>
-    </template>
   </div>
 </template>
 
@@ -19,6 +31,12 @@ import Order from './Order'
 import { mapState } from 'vuex'
 
 export default {
+  data () {
+    return {
+      // active: null
+    }
+  },
+
   props: [
     'search',
     'assays',
@@ -26,16 +44,38 @@ export default {
     'orders',
     'selection'
   ],
+
   components: {
     Inventory,
     Order
   },
+
   computed: {
     ...mapState([
       'user',
       'admin',
-      'drawer'
-    ])
+      'drawer',
+      'infoTab',
+      'activeTab'
+    ]),
+
+    active: {
+      get () {
+        return this.activeTab
+      },
+      set (value) {
+        this.$store.dispatch('setActiveTab', value)
+      }
+    }
+  },
+
+  methods: {
+    // test () {
+    //   console.log('tab test')
+    //   this.$store.dispatch('setActiveTab', 1)
+    //   this.active = parseInt(this.active) + 1
+    //   console.log(this.active)
+    // }
   }
 }
 </script>
