70fdf33a6da3bb3c1ba44f65062dd14a7ef0a2b6
rukaroa <rukaroa@gmail.com>
2018-07-17 09:32:32 -0500

    infopage for item information created

---
+++ src/components/InfoPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout>
+    <v-flex xs12>
+      <v-card>
+        <v-card-title primary-title>
+          <h3 class="headline mb-0">{{infoTab.name}}</h3>
+        </v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState([
+      'infoTab'
+    ])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
