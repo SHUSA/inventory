1fc45eb716e71426b92988c32d07fc1cc5b110a9
rukaroa <rukaroa@gmail.com>
2018-07-18 11:36:16 -0500

    moved infopage and added planned components

--- src/components/information/InfoPage.vue
+++ src/components/information/InfoPage.vue
@@ -5,6 +5,12 @@
         <v-card-title primary-title>
           <h3 class="headline mb-0">{{infoTab.name}}</h3>
         </v-card-title>
+        {{test}}
+        <slot name='test'/>
+        <slot>no content</slot>
+        <li v-for="(value, key, id) in infoTab" :key="id">
+          {{key}} : {{value}}
+        </li>
       </v-card>
     </v-flex>
   </v-layout>
@@ -14,6 +20,10 @@
 import { mapState } from 'vuex'
 
 export default {
+  props: [
+    'test'
+  ],
+
   computed: {
     ...mapState([
       'infoTab'
