885e3172f2a4f193e7f9cb29e32619e55bad1b24
rukaroa <rukaroa@gmail.com>
2018-07-19 12:21:54 -0500

    current progress; to decide display design

--- src/components/information/Item.vue
+++ src/components/information/Item.vue
@@ -1,13 +1,33 @@
 <template>
-  
+  <v-card-text slot="info">
+    <v-container fill-height grid-list-md>
+      <v-card flat dark class="box">
+        <v-card-text v-if="infoTab.itemDescription">{{infoTab.itemDescription}}</v-card-text>
+        <v-card-text v-else>No description.</v-card-text>
+      </v-card>
+    </v-container>
+    <v-container grid-list-md>
+      <v-layout wrap>
+        <v-flex xs6>{{infoTab.itemDescription}}</v-flex>
+      </v-layout>
+    </v-container>
+  </v-card-text>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
-  
+  computed: {
+    ...mapState([
+      'infoTab'
+    ])
+  }
 }
 </script>
 
 <style scoped>
-
+  .box {
+    min-width: 25%;
+  }
 </style>
