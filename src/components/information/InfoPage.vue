c43c791f4f6c214acaeb7d3561ff80bf0c6f6a20
rukaroa <rukaroa@gmail.com>
2018-07-19 12:22:50 -0500

    info page template

--- src/components/information/InfoPage.vue
+++ src/components/information/InfoPage.vue
@@ -1,19 +1,13 @@
 <template>
-  <v-layout>
-    <v-flex xs12>
-      <v-card>
-        <v-card-title primary-title>
-          <h3 class="headline mb-0">{{infoTab.name}}</h3>
-        </v-card-title>
-        {{test}}
-        <slot name='test'/>
-        <slot>no content</slot>
-        <li v-for="(value, key, id) in infoTab" :key="id">
-          {{key}} : {{value}}
-        </li>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-card>
+    <v-card-title primary-title class="pb-0">
+      <h3 class="headline mb-0">{{infoTab.name}}</h3>
+    </v-card-title>
+    <v-subheader>{{infoTab.assay}} - {{infoTab.vendor}} - Catalog #{{infoTab.catalogNumber}}</v-subheader>
+    {{test}}
+    <slot name="info"/>
+    <slot>no content</slot>
+  </v-card>
 </template>
 
 <script>
