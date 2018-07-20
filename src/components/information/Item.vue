0957f80060965df2ff7d3c8fc398f97ebd7fd564
rukaroa <rukaroa@gmail.com>
2018-07-20 10:14:29 -0500

    item initial layout

--- src/components/information/Item.vue
+++ src/components/information/Item.vue
@@ -1,17 +1,66 @@
 <template>
-  <v-card-text slot="info">
-    <v-container fill-height grid-list-md>
-      <v-card flat dark class="box">
-        <v-card-text v-if="infoTab.itemDescription">{{infoTab.itemDescription}}</v-card-text>
-        <v-card-text v-else>No description.</v-card-text>
-      </v-card>
-    </v-container>
-    <v-container grid-list-md>
-      <v-layout wrap>
-        <v-flex xs6>{{infoTab.itemDescription}}</v-flex>
-      </v-layout>
-    </v-container>
-  </v-card-text>
+  <div>
+    <v-card-title primary-title class="pb-0">
+      <h3 class="headline mb-0">{{infoTab.name}}</h3>
+      <v-chip v-if="checkQuantity" color="red lighten-3" small>QTY: {{infoTab.currentStock}}</v-chip>
+      <v-chip v-else color="blue lighten-3" small>QTY: {{infoTab.currentStock}}</v-chip>
+    </v-card-title>
+    <v-subheader>{{infoTab.assay}} - {{infoTab.vendor}} - Catalog #{{infoTab.catalogNumber}}</v-subheader>
+    <v-card-text>
+      <v-container fill-height grid-list-md>
+        <v-card flat color="transparent" class="box">
+          <v-card-text class="pa-0" v-if="infoTab.itemDescription">Description: {{infoTab.itemDescription}}</v-card-text>
+          <v-card-text class="pa-0" v-else>No description.</v-card-text>
+        </v-card>
+      </v-container>
+      <v-divider/>
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Reactions per Item</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.reactionsPerItem}}
+          </v-flex>
+
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Lead Time Days</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.leadTimeDays}}
+          </v-flex>
+
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Weeks of Safety Stock</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.weeksOfSafetyStock}}
+          </v-flex>
+
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Safety Stock</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.safetyStock}}
+          </v-flex>
+
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Weeks of Reorder</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.weeksOfReorder}}
+          </v-flex>
+
+          <v-flex xs3 fill-height justify-center>
+            <v-chip label color="light-blue lighten-1" class="label" small>Reorder Quantity</v-chip>
+          </v-flex>
+          <v-flex class="dashed-border pa-2 text-xs-right" xs3>
+            {{infoTab.reorderQuantity}}
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+  </div>
 </template>
 
 <script>
@@ -21,7 +70,11 @@ export default {
   computed: {
     ...mapState([
       'infoTab'
-    ])
+    ]),
+
+    checkQuantity () {
+      return this.infoTab.currentStock < this.infoTab.reorderPoint
+    }
   }
 }
 </script>
@@ -29,5 +82,17 @@ export default {
 <style scoped>
   .box {
     min-width: 25%;
+  }
+
+  .label {
+    width: 95%;
+  }
+
+  .dashed-border:after {
+    content: "";
+    display: block;
+    width: 105%;
+    float: right;
+    border-bottom: 1px dotted gray;
   }
 </style>
