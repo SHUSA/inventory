8a4a6b364786ca27637a4b16281ddfb05f481183
rukaroa <rukaroa@gmail.com>
2018-07-23 07:03:22 -0500

    beta build 1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar app clipped-left flat dark>
     <v-toolbar-title>
-      {{title}} v.a-build-8
+      {{title}} v.b-build-1
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items>
