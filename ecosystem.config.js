011c9d668f6fbf9e3aba03b420f65dcfa2d807c3
tmkdam <tmkdam@gmail.com>
2018-08-13 14:26:18 -0500

    Added dev env info

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,9 @@ module.exports = {
     name      : 'srl_inventory',
     script    : 'server.js',
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'development',
+      SRL_INVENTORY_HOST: '10.212.148.45',
+      PORT: 4040,
     },
     env_production : {
       NODE_ENV: 'production'
@@ -17,7 +19,7 @@ module.exports = {
       ref  : 'origin/deploy-scripts',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env development'
     },
     production : {
       user : 'rails',
