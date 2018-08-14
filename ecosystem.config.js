0b233bfb87b2e1cac8a6e4d1d3c6d81a45cdf82f
tmkdam <tmkdam@gmail.com>
2018-08-14 09:59:01 -0500

    testing pm2 deploy scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps : [{
+    name      : 'srl_inventory',
+    script    : 'server.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    development : {
+      user : 'rails',
+      host : '10.212.148.42',
+      ref  : 'pm2-deploy-script',
+      repo : 'git@github.com:SHUSA/srl_inventory.git',
+      path : '/var/www/srl_inventory',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env development',
+      "env"  : {
+        "NODE_ENV": "development",
+        SRL_INVENTORY_HOST: '10.212.148.45',
+        PORT: 4040,
+        HOST: "10.212.148.42"
+      },
+      "post-setup" : "HOST=10.212.148.42 PORT=9090 SRL_INVENTORY_HOST=10.212.148.45 npm install && NEWDB=true npm start",
+    },
+    production : {
+      user : 'rails',
+      host : '10.212.148.42',
+      ref  : 'origin/deploy-scripts',
+      repo : 'git@github.com:SHUSA/srl_inventory.git',
+      path : '/var/www/srl_inventory',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      "env"  : {
+        "NODE_ENV": "production"
+      }
+    }
+  }
+};
