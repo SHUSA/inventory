ea0e2a35e7db49683a3b5f43d4885115fc5df220
tmkdam <tmkdam@gmail.com>
2018-08-14 10:54:32 -0500

    Fixing server config issues

--- server.js
+++ server.js
@@ -24,12 +24,13 @@ app.get('*', function (req, res) {
 
 if (process.env.NEWDB) {
   sequelize.sync({ force: false }).then(() => {
-    app.listen(config.port)
+    app.listen(config.port, config.host)
     if (process.env.NEWDB) console.log('Tables have been formatted')
     console.log(`Server started on port ${config.port}`)
   })  
+}else{
+  app.listen(config.port, config.host, () => {
+    console.log(`Server started on port ${config.port}`)
+  })
 }
 
-app.listen(config.port, config.host, () => {
-  console.log(`Server started on port ${config.port}`)
-})
