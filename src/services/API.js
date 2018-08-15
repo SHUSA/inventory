c212e1fa04babc1989345c487dcb52cc43780ce9
tmkdam <tmkdam@gmail.com>
2018-08-15 08:26:36 -0500

    Updating config paths

--- src/services/API.js
+++ src/services/API.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+const config = require('../../server/config/config')
 // for dev purposes
 // baseURL: process.env.HOST || 'http://localhost:8081/'
+
+let baseURL = `${config.host}:${config.port}` || 'http://localhost:8081/'
 export default () => {
   return axios.create({
-    baseURL:  process.env.HOST || 'http://localhost:8081/'
+    baseURL: baseURL
   })
 }
