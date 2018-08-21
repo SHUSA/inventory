a048b0285c28b683decdba2b560e0e5d7b47d150
rukaroa <rukaroa@gmail.com>
2018-08-21 07:05:55 -0500

    url fix for local hosting

--- src/services/API.js
+++ src/services/API.js
@@ -3,7 +3,9 @@ const config = require('../../server/config/config')
 // for dev purposes
 // baseURL: process.env.HOST || 'http://localhost:8081/'
 
-let baseURL = `${config.host}:${config.port}` || 'http://localhost:8081/'
+let baseURL = `${config.host}:${config.port}`
+baseURL = baseURL === 'localhost:8081' ? 'http://' + baseURL : baseURL
+
 export default () => {
   return axios.create({
     baseURL: baseURL
