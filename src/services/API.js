b9c0f16307a18dc7c14241a4cb6579852ff74acd
rukaroa <rukaroa@gmail.com>
2018-08-22 09:19:55 -0500

    new build to include recent changes

--- src/services/API.js
+++ src/services/API.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 const config = require('../../server/config/config')
-// for dev purposes
-// baseURL: process.env.HOST || 'http://localhost:8081/'
 
 let baseURL = `${config.host}:${config.port}`
 baseURL = baseURL === 'localhost:8081' ? 'http://' + baseURL : baseURL
