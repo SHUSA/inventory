13459310e3e65adf811dd7455b4cb2a863fef70b
rukaroa <rukaroa@gmail.com>
2018-09-07 10:49:59 -0500

    list component base

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs2 v-for="(item, index) in list" :key="item.name">
+          <v-chip label>{{index}} - {{item.name}}</v-chip>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'list'
+  ]
+}
+</script>
+
+<style scoped>
+
+</style>
