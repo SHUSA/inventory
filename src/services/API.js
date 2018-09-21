4cddf1dafb7f4102113375c4609f638525a7872b
rukaroa <rukaroa@gmail.com>
2018-09-21 09:22:14 -0500

    added http:// to env and localhost

--- src/services/API.js
+++ src/services/API.js
@@ -2,15 +2,9 @@ import axios from 'axios'
 
 // env taken from webpack
 const port = process.env.PORT || 8081
-const host = process.env.HOST || 'http://localhost'
+const host = 'http://' + (process.env.HOST || 'localhost')
 
 let baseURL = `${host}:${port}`
-
-console.log('axios')
-console.log(`NODE_ENV ${process.env.NODE_ENV}`)
-console.log(`PORT ${process.env.PORT}`)
-console.log(`HOST ${process.env.HOST}`)
-console.log(baseURL)
 
 export default () => {
   return axios.create({
