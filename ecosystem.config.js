6b77dce0de8be7e76866d8660998043ea9506ad5
rukaroa <rukaroa@gmail.com>
2018-09-24 10:55:10 -0500

    adding npm start to post-deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref  : 'origin/beta',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env',
+      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
       "env"  : {
         'NODE_ENV': "development",
         SRL_INVENTORY_HOST: '10.212.148.45',
@@ -25,7 +25,7 @@ module.exports = {
         PORT: 4040,
         SERVER_PORT: 4041,
         HOST: "10.212.148.42",
-        NEWDB: true
+        NEWDB: false
       },
       "post-setup": "HOST=10.212.148.42 PORT=4040 SRL_INVENTORY_HOST=10.212.148.45 npm install && NEWDB=true npm start",
     },
