2e7f548758e7d83b01460cd8d2ec3cb67a99324b
rukaroa <rukaroa@gmail.com>
2018-09-24 11:02:25 -0500

    adding envs to dev webpack

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  PORT: JSON.stringify(process.env.PORT),
-  HOST: JSON.stringify(process.env.SRL_INVENTORY_HOST)
+  PORT: JSON.stringify(process.env.SERVER_PORT),
+  HOST: JSON.stringify(process.env.HOST),
 })
