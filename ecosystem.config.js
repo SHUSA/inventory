7fe28501c4fac8a7f861f1a31f5d31a0584c7cb6
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-16 06:29:13 -0500

    checking processes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,8 @@ module.exports = {
       ref  : 'origin/beta',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-envt',
+      // 'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
+      'post-deploy': 'pm2 list && pm2 show 0 && pm2 show 1',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
