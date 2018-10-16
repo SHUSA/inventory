cfc9586b8b3610824ee5e6549c2d560b921c1e7f
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-16 06:09:02 -0500

    fixing command

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
       // 'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
-      'post-deplot': 'pm2 kill-0',
+      'post-deplot': 'pm2 kill 0',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
