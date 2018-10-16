352129140fc8cc3153bb89ecec00386f8a1b1415
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-16 06:14:49 -0500

    relaunch site at 4040

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,8 +17,7 @@ module.exports = {
       ref  : 'origin/beta',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      // 'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
-      'post-deploy': 'git pull && pm2 kill 0',
+      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
