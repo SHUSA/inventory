6762d7ce1ad70c29d5e46ee0323b80b11e1551f2
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-17 11:31:23 -0500

    trying to start index 0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
       // 'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list',
-      'post-deploy': 'git pull && pm2 list',
+      'post-deploy': 'git pull && pm2 start 0',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
