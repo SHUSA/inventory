7121cb9cd9683a8520156bbaca0f89b4f4411527
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-17 07:29:44 -0500

    trying code change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,18 +17,18 @@ module.exports = {
       ref  : 'origin/beta',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm start',
+      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && node server.js',
       // 'post-deploy': 'git pull && /usr/sbin/lsof -w -n -i tcp:4041',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
         SRL_INVENTORY_DB_PORT: 5432,
         PORT: 4040,
-        SERVER_PORT: 4042,
+        SERVER_PORT: 4041,
         HOST: "10.212.148.42",
         NEWDB: "'false'"
       },
-      "post-setup": "HOST=10.212.148.42 PORT=4040 SRL_INVENTORY_HOST=10.212.148.45 npm install && npm start"
+      "post-setup": "HOST=10.212.148.42 PORT=4040 SRL_INVENTORY_HOST=10.212.148.45 npm install &&  node server.js"
     },
     production : {
       user : 'rails',
