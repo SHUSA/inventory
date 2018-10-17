73f70890e0c09a5d168a25e27b388fb5827dd59e
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-17 11:00:12 -0500

    checking

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
       // 'post-deploy': 'git pull && npm install && pm2 reload ecosystem.config.js --env development --update-env && node server.js',
-      'post-deploy': 'git pull && pm2 list && pm2 start server.js',
+      'post-deploy': 'git pull && pm2 list && pm2 start all',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <v-toolbar app clipped-left flat dark>
     <v-toolbar-title>
-      {{title}} v.b-1.7.13
+      {{title}} v.b-1.7.14
       <v-dialog
         v-model="dialog"
         scrollable
