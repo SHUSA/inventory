40cc5775439aef6a622088203b6780b9dfd386b0
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-10-17 10:13:17 -0500

    attempting to redeploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref  : 'origin/beta',
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
-      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && npm run deploy',
+      'post-deploy': 'git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && node server.js',
       // 'post-deploy': 'git pull && pm2 start all',
       "env"  : {
         'NODE_ENV': "'development'",
@@ -28,7 +28,7 @@ module.exports = {
         HOST: "10.212.148.42",
         NEWDB: "'false'"
       },
-      "post-setup": "HOST=10.212.148.42 PORT=4040 SRL_INVENTORY_HOST=10.212.148.45 npm install && npm run deploy"
+      "post-setup": "HOST=10.212.148.42 PORT=4040 SRL_INVENTORY_HOST=10.212.148.45 npm install && node server.js"
     },
     production : {
       user : 'rails',
