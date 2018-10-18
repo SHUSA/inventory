b3b994f813caf105f13c4f5f81873611bb5bc48c
rukaroa <rukaroa@gmail.com>
2018-10-18 06:19:01 -0500

    removing console

--- src/services/API.js
+++ src/services/API.js
@@ -4,10 +4,6 @@ import axios from 'axios'
 const port = process.env.SERVER_PORT || 8081
 const host = 'http://' + (process.env.HOST || 'localhost')
 
-console.log('api.js')
-console.log(`server port ${port}`)
-console.log(`regular port ${process.env.PORT}`)
-
 let baseURL = `${host}:${port}`
 
 export default () => {
