b04c48e7ca644996c8c4b3ce34c995fb64c28e87
rukaroa <rukaroa@gmail.com>
2018-10-18 06:11:34 -0500

    fix and troubleshooting

--- src/services/API.js
+++ src/services/API.js
@@ -4,6 +4,10 @@ import axios from 'axios'
 const port = process.env.SERVER_PORT || 8081
 const host = 'http://' + (process.env.HOST || 'localhost')
 
+console.log('api.js')
+console.log(`server port ${port}`)
+console.log(`regular port ${process.env.PORT}`)
+
 let baseURL = `${host}:${port}`
 
 export default () => {
