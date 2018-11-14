9c88fe5d4033010dc4b5e7f21d409e7fe3615c36
rukaroa <rukaroa@gmail.com>
2018-11-14 10:39:02 -0600

    planned base component

---
+++ src/components/inventory/InventoryIndex.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <!-- use this component to pre-select options before rendering table -->
+    <!-- options: choose assays and/or vendors; some, one, or all -->
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
