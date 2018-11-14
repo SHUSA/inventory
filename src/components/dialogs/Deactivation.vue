f327a6bd25a771d1d44b1760e46fdcee8857cfc4
rukaroa <rukaroa@gmail.com>
2018-11-14 12:28:23 -0600

    planned dialog

---
+++ src/components/dialogs/Deactivation.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog
+    v-model="deactivationDialog"
+    max-width="500px"
+  >
+    <v-card>
+      <v-card-title>
+        <span class="headline">Deactivate {{currentItem.name}}?</span>
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn color="blue darken-1" flat @click="deactivationDialog = false">No</v-btn>
+        <v-btn color="red darken-1" flat @click="deactivateItem(currentItem)">Yes</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
