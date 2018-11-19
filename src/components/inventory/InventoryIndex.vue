9224d870d2e35b0690323e12bd5c4863ed72cf45
rukaroa <rukaroa@gmail.com>
2018-11-19 12:16:42 -0600

    use id in selected arr; add transitions

--- src/components/inventory/InventoryIndex.vue
+++ src/components/inventory/InventoryIndex.vue
@@ -8,17 +8,24 @@
         <v-btn @click="populateList('assays')">Assays</v-btn>
         <v-btn @click="populateList('vendors')">Vendors</v-btn>
         <v-flex xs12>
-          <transition-group name="chips" tag="span">
-            <v-chip v-if="show" key="select-all" @click="selectAll(list)" :color="allSelected ? 'red' : someSelected ? 'orange' : ''">
+          <!-- select all -->
+          <transition name="chips">
+            <v-chip v-if="show"  @click="selectAll(list)" :color="allSelected ? 'red' : someSelected ? 'orange' : ''">
               {{!allSelected ? 'SELECT ALL' : 'DESELECT ALL'}}
             </v-chip>
-            <v-chip v-if="show" key="submit" @click="submit = true">Go!</v-chip>
-            <br key="break">
+          </transition>
+          <!-- submit -->
+          <transition name="submit" mode="out-in">
+            <v-chip v-if="allSelected || someSelected" key="submit" @click="submit = true" color="green">Go!</v-chip>
+          </transition>
+          <br>
+          <!-- filter chips -->
+          <transition-group name="chips" tag="span">
             <v-chip
               v-if="show"
               v-for="(item, index) in list"
               :key="index"
-              :color="isSelected(item.name) ? 'blue lighten-2' : ''"
+              :color="isSelected(item.id) ? 'blue lighten-2' : ''"
               @click="add(item)"
             >
               {{item.name}}
@@ -26,6 +33,7 @@
           </transition-group>
         </v-flex>
       </v-layout>
+      <!-- inventory -->
       <inventory v-else :selected="selected"/>
     </v-container>
   </v-card>
@@ -73,14 +81,18 @@ export default {
 
   methods: {
     async initialize () {
-      this.assays = (await assayService.index(true, ['name'])).data
-      this.vendors = (await vendorService.index(true, ['name'])).data
+      this.assays = (await assayService.index(true, ['name', 'id'])).data
+      this.vendors = (await vendorService.index(true, ['name', 'id'])).data
     },
 
     populateList (type) {
       // control chip visibility
       if (this.show) {
-        if (this.shown === type) this.show = false
+        if (this.shown === type) {
+          this.show = false
+        } else {
+          this.selected = []
+        }
       } else {
         this.show = true
       }
@@ -96,10 +108,10 @@ export default {
 
     add (item) {
       let arr = this.selected
-      if (!arr.includes(item.name)) {
-        arr.push(item.name)
+      if (!arr.includes(item.id)) {
+        arr.push(item.id)
       } else {
-        arr.splice(arr.indexOf(item.name), 1)
+        arr.splice(arr.indexOf(item.id), 1)
       }
     },
 
@@ -111,7 +123,7 @@ export default {
       if (this.selected.length === list.length) {
         this.selected = []
       } else {
-        this.selected = list.map(x => x.name)
+        this.selected = list.map(x => x.id)
       }
     }
   }
@@ -132,5 +144,13 @@ export default {
   .chips-leave-to {
     opacity: 0;
     transform: translateX(100px);
+  }
+
+  .submit-enter-active, .submit-leave-active {
+    transition: opacity .3s ease;
+  }
+
+  .submit-enter, .submit-leave-to {
+    opacity: 0;
   }
 </style>
