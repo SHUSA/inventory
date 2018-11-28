1f3123a0007d9211288aeaaa808183679a9b5ccb
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2018-11-28 10:37:19 -0600

    trying to restart server with new code

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : '/var/www/srl_inventory',
       // 'post-deploy': 'git pull && pm2 list && pm2 expose',
-      'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
+      'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && npm start && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
