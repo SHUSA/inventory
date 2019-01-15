18cc1e9f738b7746e67951ff9dbf993bd633654a
rukaroa <rukaroa@gmail.com>
2019-01-15 06:43:18 -0600

    added assay info; removes selected and associated data from lists

--- src/components/information/Catalog.vue
+++ src/components/information/Catalog.vue
@@ -2,6 +2,7 @@
   <v-card v-if="loadComponent" flat color="transparent">
     <deactivation :selection.sync="selectedChip" :dialog.sync="dialog" :reassigned.sync="reassigned" :assays="assays" :vendors="vendors"/>
     <item-info v-if="itemInfoDialog" :item.sync="selectedChip" :dialog.sync="itemInfoDialog" :assays="assays" :vendors="vendors"/>
+    <assay-info v-if="assayInfoDialog" :assay.sync="selectedChip" :dialog.sync="assayInfoDialog" />
     <error :response="response"/>
     <v-container fill-height grid-list-md>
       <v-layout row wrap>
@@ -12,7 +13,7 @@
         <v-flex xs4 v-for="(list, index) in lists" :key="index">
           <v-list dense class="transparent">
             <v-list-tile-title class="title">{{list}}</v-list-tile-title>
-            <v-chip v-if="item.active" class="truncate" v-for="item in getArray(list)" :key="item.id" :color="item.hasItem ? 'info' : ''" @click="select(item)">
+            <v-chip class="truncate" v-for="item in getArray(list)" :key="item.id" :color="item.hasItem ? 'info' : ''" @click="select(item)">
               {{item.name}}
             </v-chip>
           </v-list>
@@ -42,6 +43,7 @@ import assayService from '@/services/AssayService.js'
 import vendorService from '@/services/VendorService.js'
 import Deactivation from '../dialogs/Deactivation'
 import ItemInfo from '../information/ItemInfo'
+import AssayInfo from '../information/AssayInfo'
 
 export default {
   data () {
@@ -55,6 +57,7 @@ export default {
       selectedChip: {},
       dialog: false,
       itemInfoDialog: false,
+      assayInfoDialog: false,
       snackbar: false,
       lists: ['Items', 'Assays', 'Vendors']
     }
@@ -69,17 +72,65 @@ export default {
   },
 
   watch: {
+    selectedChip (val) {
+      let index = null
+      // remove items if their assay or vendor is deactivated
+      if (val.hasOwnProperty('catalogNumber') && !val.active) {
+        // remove inactive items from item list
+        index = this.items.findIndex(item => item.id === val.id)
+        this.items.splice(index, 1)
+      } else if (val.hasOwnProperty('weeklyVolume') && !val.active) {
+        // is assay
+        if (val.hasItem) {
+          // remove items with same assay id
+          for (let i = this.items.length - 1; i >= 0; i--) {
+            if (this.items[i].AssayId === val.id) {
+              this.items.splice(i, 1)
+            }
+          }
+        }
+        index = this.assays.findIndex(assay => assay.id === val.id)
+        this.assays.splice(index, 1)
+      } else if (!val.active) {
+        // is vendor
+        if (val.hasItem) {
+          // remove items with same vendor id
+          for (let i = this.items.length - 1; i >= 0; i--) {
+            if (this.items[i].VendorId === val.id) {
+              this.items.splice(i, 1)
+            }
+          }
+        }
+        index = this.vendors.findIndex(vendor => vendor.id === val.id)
+        this.vendors.splice(index, 1)
+      }
+    },
+
     reassigned (val) {
       if (val.hasOwnProperty('weeklyVolume')) {
+        // change all items with associated assay
         this.assays.find(assay => {
           if (assay.id === val.id) {
             assay.hasItem = val.hasItem
           }
         })
+        this.items.forEach(item => {
+          if (item.AssayId === this.selectedChip.id) {
+            item.AssayId = val.id
+            item.AssayName = val.name
+          }
+        })
       } else {
+        // change all items with associated vendor
         this.vendors.find(vendor => {
           if (vendor.id === val.id) {
             vendor.hasItem = val.hasItem
+          }
+        })
+        this.items.forEach(item => {
+          if (item.VendorId === this.selectedChip.id) {
+            item.VendorId = val.id
+            item.VendorName = val.name
           }
         })
       }
@@ -88,7 +139,8 @@ export default {
 
   components: {
     Deactivation,
-    ItemInfo
+    ItemInfo,
+    AssayInfo
   },
 
   mounted () {
@@ -98,8 +150,6 @@ export default {
 
   methods: {
     async initialize () {
-      let itemAssayIds = {}
-      let itemVendorIds = {}
       this.$store.dispatch('setTitle', 'Catalog')
       this.response = (await itemService.index())
 
@@ -108,24 +158,31 @@ export default {
         this.items = this.response.data
         this.assays = (await assayService.index()).data
         this.vendors = (await vendorService.index()).data
-        // to do: find if assay has an assigned item
-        // store assay and vendor ids from each item
-        this.items.forEach(item => {
-          itemAssayIds[item.AssayId] = true
-          itemVendorIds[item.VendorId] = true
-        })
-        // loop through assays and vendors and check if id is in the object ids
-        this.assays.forEach(assay => {
-          assay.hasItem = itemAssayIds.hasOwnProperty(assay.id)
-        })
-        this.vendors.forEach(vendor => {
-          vendor.hasItem = itemVendorIds.hasOwnProperty(vendor.id)
-        })
+
+        this.hasItem()
       }
       // go to top
       window.scroll({
         top: 0,
         left: 0
+      })
+    },
+
+    hasItem () {
+      let itemAssayIds = {}
+      let itemVendorIds = {}
+
+      // store assay and vendor ids from each item
+      this.items.forEach(item => {
+        itemAssayIds[item.AssayId] = true
+        itemVendorIds[item.VendorId] = true
+      })
+      // loop through assays and vendors and check if id is in the object ids
+      this.assays.forEach(assay => {
+        assay.hasItem = itemAssayIds.hasOwnProperty(assay.id)
+      })
+      this.vendors.forEach(vendor => {
+        vendor.hasItem = itemVendorIds.hasOwnProperty(vendor.id)
       })
     },
 
@@ -136,9 +193,12 @@ export default {
     select (obj) {
       this.selectedChip = obj
 
-      if (obj.hasOwnProperty('catalogNumber')) {
-        this.itemInfoDialog = true
-      } else if (this.admin) {
+      // if (obj.hasOwnProperty('catalogNumber')) {
+      //   this.itemInfoDialog = true
+      // } else if (obj.hasOwnProperty('weeklyVolume')) {
+      //   this.assayInfoDialog = true
+      // } else
+      if (this.admin) {
         // deactivation dialog
         this.dialog = true
       } else {
