b5f19f224bc1c44264af8ae8b09d102aa9f52074
rukaroa <rukaroa@gmail.com>
2019-01-16 10:52:56 -0600

    refactored completed orders to be divided by month

--- src/components/order/OrderIndex.vue
+++ src/components/order/OrderIndex.vue
@@ -6,38 +6,56 @@
       <!-- incomplete -->
       <v-expansion-panel-content :class="panelClass(0)">
         <div slot="header">
-          <v-icon small>fa-list-ul</v-icon>
+          <v-icon>fa-clipboard-list</v-icon>
           Incomplete Orders
         </div>
         <v-divider/>
-        <v-list>
+        <v-list v-if="incompleteOrders.length > 0">
           <v-list-tile v-for="order in incompleteOrders" :key="order.createdAt" @click="viewOrder(order)">
             <v-list-tile-action>
               <v-icon>fa-angle-right</v-icon>
             </v-list-tile-action>
-            <v-list-tile-title v-if="order.new">{{order.name}}</v-list-tile-title>
-            <v-list-tile-title v-else>Week of {{time(order.createdAt)}}</v-list-tile-title>
+            <v-list-tile-title>Week of {{time(order.createdAt)}}</v-list-tile-title>
           </v-list-tile>
-          <v-alert :value="incompleteOrders.length === 0" color="success" icon="fa-check">All caught up!</v-alert>
         </v-list>
+        <v-alert :value="incompleteOrders.length === 0" color="success" icon="fa-check">All caught up!</v-alert>
       </v-expansion-panel-content>
       <!-- completed -->
       <v-expansion-panel-content :class="panelClass(1)">
         <div slot="header">
-          <v-icon small>fa-check</v-icon>
+          <v-icon>fa-check-double</v-icon>
           Completed Orders
         </div>
         <v-divider/>
-        <v-list>
-          <v-list-tile v-for="order in completedOrders" :key="order.createdAt" @click="viewOrder(order)">
-            <v-list-tile-action>
-              <v-icon>fa-check</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-title v-if="order.new">{{order.name}}</v-list-tile-title>
-            <v-list-tile-title v-else>Week of {{time(order.createdAt)}}</v-list-tile-title>
-          </v-list-tile>
-          <v-alert :value="completedOrders.length === 0" color="error" icon="fa-exclamation-triangle">No orders to display!</v-alert>
+        <v-list  v-if="completedOrders.length > 0">
+          <v-list-group
+            v-for="group in completedOrders"
+            v-model="group.active"
+            :key="group.title"
+            prepend-icon="fa-calendar-alt"
+            no-action
+          >
+            <v-list-tile slot="activator">
+              <v-list-tile-content>
+                <v-list-tile-title>{{group.title}}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-list-tile
+              v-for="order in group.orders"
+              :key="order.createdAt"
+              @click="viewOrder(order)"
+            >
+              <v-list-tile-action>
+                <v-icon>fa-check</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>Week of {{time(order.createdAt)}}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
         </v-list>
+        <v-alert :value="completedOrders.length === 0" color="error" icon="fa-exclamation-triangle">No orders to display!</v-alert>
       </v-expansion-panel-content>
     </v-expansion-panel>
   </v-card>
@@ -95,12 +113,36 @@ export default {
       this.incompleteOrders = []
 
       if (this.response.status === 200) {
+        let i = 0
         this.orders = [...this.response.data]
-        this.orders.forEach(order => {
-          if (order.completed) {
-            this.completedOrders.push(order)
-          } else {
+
+        this.orders.forEach((order, index) => {
+          if (!order.completed) {
             this.incompleteOrders.push(order)
+          } else {
+            if (this.completedOrders.length > 0) {
+              // compare previous create date with new create date
+              let orderGroup = this.completedOrders[i]
+              if (this.$moment(order.createdAt).isSame(orderGroup.orders[0].createdAt, 'month')) {
+                // if same month, push to current index
+                orderGroup.orders.push(order)
+              } else {
+                // else push to next index
+                this.completedOrders.push({
+                  title: this.$moment(order.createdAt).startOf('week').format('MMMM YYYY'),
+                  orders: []
+                })
+                i += 1
+                this.completedOrders[i].orders.push(order)
+              }
+            } else {
+              // create 'month - year' label for group title
+              this.completedOrders.push({
+                title: this.$moment(order.createdAt).startOf('week').format('MMMM YYYY'),
+                orders: []
+              })
+              this.completedOrders[i].orders.push(order)
+            }
           }
         })
       }
