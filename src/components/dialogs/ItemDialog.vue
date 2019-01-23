3bff73ecd8297edef644952b0de13ff835d89af2
rukaroa <rukaroa@gmail.com>
2019-01-23 09:32:32 -0600

    item dialog reworked; to do assay and vendor routes

--- src/components/dialogs/ItemDialog.vue
+++ src/components/dialogs/ItemDialog.vue
@@ -1,21 +1,28 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    max-width="500px"
-    @keydown.enter="save(false)"
-  >
-    <!-- <v-btn v-if="admin" slot="activator" color="primary" class="mb-0" dark>New Item</v-btn> -->
-    <v-card>
-      <v-card-title>
-        <span class="headline">{{formTitle}}</span>
-      </v-card-title>
-      <v-card-text>
-        <v-container grid-list-md>
-          <v-layout wrap>
-            <!-- admin input -->
-            <template v-if="admin">
+  <div>
+    <error :response="response"/>
+      <!-- snack -->
+    <v-snackbar
+      v-model="snackbar"
+      color="primary"
+      bottom
+    >
+      <v-flex class="text-xs-center">
+        {{snackText}}
+      </v-flex>
+    </v-snackbar>
+
+    <!-- dialog -->
+    <v-dialog
+      v-model="dialog"
+      max-width="500px"
+    >
+      <dialog-base :formTitle="formTitle">
+        <v-form slot="input-fields" ref="form" v-model="form" lazy-validation>
+          <v-container>
+            <v-layout row wrap>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.name" :rules="[rules.item]" label="Item Name" required/>
+                <v-text-field v-model.trim="editedItem.name" :rules="[rules.item]" label="Item Name" clearable required/>
               </v-flex>
               <v-flex xs6>
                 <v-autocomplete
@@ -24,7 +31,7 @@
                   item-text="name"
                   item-value="id"
                   v-model="editedItem.AssayId"
-                  append-icon="fa-plus-circle "
+                  append-icon="fa-plus-square"
                   @click:append="addAssay"
                   :rules="[rules.assay]"
                   dense
@@ -38,7 +45,7 @@
                   item-text="name"
                   item-value="id"
                   v-model="editedItem.VendorId"
-                  append-icon="fa-plus-circle "
+                  append-icon="fa-plus-square"
                   @click:append="addVendor"
                   :rules="[rules.vendor]"
                   dense
@@ -46,149 +53,329 @@
                 />
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.catalogNumber" :rules="[rules.catalog]" label="Catalog Number" required/>
+                <v-text-field v-model.trim="editedItem.catalogNumber" :rules="[rules.catalog]" label="Catalog Number" clearable required/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.itemDescription" label="Item Description"/>
+                <v-text-field v-model.trim="editedItem.itemDescription" label="Item Description" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.reactionsPerItem" validate-on-blur :rules="[rules.number]" ref="reactionsPerItem" type="number" min=0 hint="Use 0 for general items." persistent-hint label="Reactions per Item"/>
+                <v-text-field v-model.number="editedItem.reactionsPerItem" validate-on-blur :rules="[rules.number]" ref="reactionsPerItem" type="number" min=0 hint="Use 0 for general items." persistent-hint label="Reactions per Item" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.currentStock" validate-on-blur :rules="[rules.number]" ref="currentStock" type="number" min=0 label="Current Stock"/>
+                <v-text-field v-model.number="editedItem.currentStock" validate-on-blur :rules="[rules.number]" ref="currentStock" type="number" min=0 label="Current Stock" clearable/>
               </v-flex>
               <v-flex xs6>
                 <v-text-field disabled v-model="editedItem.safetyStock" label="Safety Stock"/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.weeksOfSafetyStock" validate-on-blur :rules="[rules.wholeNumber]" ref="safetyStock" type="number" min=0 label="Safety Weeks"/>
+                <v-text-field v-model.number="editedItem.weeksOfSafetyStock" validate-on-blur :rules="[rules.number]" ref="weeksOfSafetyStock" type="number" min=0 label="Safety Weeks" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.leadTimeDays" validate-on-blur :rules="[rules.wholeNumber]" ref="leadtimeDays" type="number" min=0 label="Lead Time (Days)"/>
+                <v-text-field v-model.number="editedItem.leadTimeDays" validate-on-blur :rules="[rules.number]" ref="leadtimeDays" type="number" min=0 label="Lead Time (Days)" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.weeksOfReorder" validate-on-blur :rules="[rules.wholeNumber]" ref="weeksOfReorder" type="number" min=0 label="Reorder Weeks"/>
+                <v-text-field v-model.number="editedItem.weeksOfReorder" validate-on-blur :rules="[rules.number]" ref="weeksOfReorder" type="number" min=0 label="Reorder Weeks" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.reorderPoint" validate-on-blur :rules="[rules.number]" ref="reorderPoint" type="number" min=0 label="Reorder Point"/>
+                <v-text-field v-model.number="editedItem.reorderPoint" validate-on-blur :rules="[rules.number]" ref="reorderPoint" type="number" min=0 label="Reorder Point" clearable/>
               </v-flex>
               <v-flex xs6>
-                <v-text-field v-model="editedItem.reorderQuantity" validate-on-blur :rules="[rules.wholeNumber]" ref="reorderQuantity" type="number" min=0 label="Reorder Quantity"/>
+                <v-text-field v-model.number="editedItem.reorderQuantity" validate-on-blur :rules="[rules.wholeNumber]" ref="reorderQuantity" type="number" min=0 label="Reorder Quantity" clearable/>
               </v-flex>
-            </template>
-            <!-- user input -->
-            <template v-if="user">
-              <v-flex xs6 fill-height justify-center>
-                <v-chip label color="light-blue lighten-1" class="label" small>Reactions per Item</v-chip>
-              </v-flex>
-              <v-flex class="dashed-border pa-2 text-xs-right" xs6>
-                {{editedItem.reactionsPerItem}}
-              </v-flex>
-
-              <v-flex xs6 fill-height justify-center>
-                <v-chip label color="light-blue lighten-1" class="label" small>Reorder Point</v-chip>
-              </v-flex>
-              <v-flex class="dashed-border pa-2 text-xs-right" xs6>
-                {{editedItem.reorderPoint}}
-              </v-flex>
-
-              <v-flex xs6 fill-height justify-center>
-                <v-chip label color="light-blue lighten-1" class="label" small>Reorder Quantity</v-chip>
-              </v-flex>
-              <v-flex class="dashed-border pa-2 text-xs-right" xs6>
-                {{editedItem.reorderQuantity}}
-              </v-flex>
-
-              <v-flex xs12>
-                <v-text-field
-                  v-model="editedItem.currentStock"
-                  validate-on-blur :rules="[rules.number]"
-                  ref="focus"
-                  type="number"
-                  min=0
-                  label="Current Stock"
-                  persistent-hint
-                />
-              </v-flex>
-              <v-flex xs12>
-                <v-textarea
-                v-model="editedItem.comment"
-                outline
-                no-resize
-                counter=140
-                validate-on-blur
-                :rules="[rules.text]"
-                label="Comment"/>
-              </v-flex>
-            </template>
-            <v-flex xs12>
-              <v-alert
-                :value="alert"
-                type="error"
-              >
-                {{alertMessage}}
-              </v-alert>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <!-- enable deactivation button once reactivation is complete -->
-        <v-btn color="red darken-1" disabled @click.native="deactivationDialog = !deactivationDialog" v-if="currentItem.name && admin">Deactivate</v-btn>
-        <v-btn color="orange" small @click.native="save(true)" v-if="user">Manual Order</v-btn>
-        <v-spacer/>
-        <v-progress-circular indeterminate color="primary" v-if="loading"/>
-        <v-btn color="red darken-1" flat @click.native="close">Cancel</v-btn>
-        <v-btn color="blue darken-1" flat @click.native="save(false)">Save</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+            </v-layout>
+          </v-container>
+        </v-form>
+        <!-- alert -->
+        <v-flex xs12>
+          <v-alert
+            :value="alert"
+            type="error"
+          >
+            {{alertMessage}}
+          </v-alert>
+        </v-flex>
+        <!-- actions -->
+        <template slot="buttons">
+          <v-progress-circular indeterminate color="primary" v-if="loading"/>
+          <v-btn flat color="error">Cancel</v-btn>
+          <v-btn flat color="primary" @click="validateData()">Save</v-btn>
+        </template>
+      </dialog-base>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import itemService from '@/services/ItemService.js'
-import assayService from '@/services/AssayService.js'
-import vendorService from '@/services/VendorService.js'
-import entryService from '@/services/EntryService.js'
-import orderService from '@/services/OrderService.js'
 import AssayDialog from './AssayDialog'
 import VendorDialog from './VendorDialog'
-// Notes on number input type
-// -unable to block e, -, +
-// -unable to prevent mouse scroll
-// -possible vue limitation?
-// -look into https://www.npmjs.com/package/@paulpv/vuetify-number-field?activeTab=readme
-// in the meantime, treat as text and only allow real and positive numbers to pass through
+import Deactivation from './Deactivation'
+import DialogBase from './DialogBase'
 
 export default {
   props: [
+    'itemDialog',
+    'selectedItem',
+    'catalogNumbers',
     'itemList',
     'assayList',
-    'vendorList'
+    'vendorList',
+    'editedIndex'
   ],
-
-  computed: {
-    ...mapState([
-      'admin',
-      'user',
-      'itemDialog',
-      'assayDialog',
-      'vendorDialog',
-      'itemSave',
-      'assaySave',
-      'vendorSave'
-    ])
-  },
 
   components: {
     AssayDialog,
-    VendorDialog
+    VendorDialog,
+    DialogBase,
+    Deactivation
   },
 
   data () {
     return {
-      
+      form: true,
+      deactivationDialog: false,
+      response: '',
+      alert: false,
+      alertMessage: '',
+      loading: false,
+      snackbar: false,
+      snackText: '',
+      formTitle: 'New Item',
+      rules: {
+        number: (val) => {
+          return val !== null && val >= 0 ? true : 'Please enter a valid number'
+        },
+        wholeNumber: (val) => {
+          return val !== null && val >= 0 && Number.isInteger(val) ? true : 'Please enter an integer'
+        },
+        catalog: (text) => {
+          if (text.length === 0) {
+            return 'Please enter a unique catalog number'
+          } else if (this.catalogNumbers.includes(text.toUpperCase())) {
+            // fixes error throwing on existing items
+            if (this.index > -1) {
+              return true
+            } else {
+              return 'Duplicate catalog found'
+            }
+          } else {
+            return true
+          }
+        },
+        assay: (text) => {
+          if (text.length === 0) {
+            return 'Please enter a valid name'
+          } else if (this.assayList.find(assay => assay.name.toUpperCase() === text.toUpperCase())) {
+            if (this.index > -1) {
+              return true
+            } else {
+              return 'Duplicate assay name found'
+            }
+          } else {
+            return true
+          }
+        },
+        vendor: (text) => {
+          if (text.length === 0) {
+            return 'Please enter a valid name'
+          } else if (this.vendorList.find(vendor => vendor.name.toUpperCase() === text.toUpperCase())) {
+            if (this.index > -1) {
+              return true
+            } else {
+              return 'Duplicate vendor name found'
+            }
+          } else {
+            return true
+          }
+        },
+
+        item: (text) => {
+          return text.length > 0 ? true : 'Please enter a valid name'
+        }
+
+      },
+      editedItem: {
+        name: '',
+        AssayId: '',
+        VendorId: '',
+        catalogNumber: '',
+        itemDescription: '',
+        reactionsPerItem: 0,
+        currentStock: 0,
+        weeksOfSafetyStock: 2,
+        leadTimeDays: 7,
+        weeksOfReorder: 8,
+        reorderPoint: 0,
+        reorderQuantity: 0
+      },
+      defaultItem: {
+        name: '',
+        AssayId: '',
+        VendorId: '',
+        catalogNumber: '',
+        itemDescription: '',
+        reactionsPerItem: 0,
+        currentStock: 0,
+        weeksOfSafetyStock: 2,
+        leadTimeDays: 7,
+        weeksOfReorder: 6,
+        reorderPoint: 0,
+        reorderQuantity: 0
+      }
+    }
+  },
+
+  watch: {
+    dialog (val) {
+      if (val) {
+        this.editedItem = Object.assign(this.editedItem, this.selectedItem)
+        this.formTitle = this.index === -1 ? 'New Item' : `Editing ${this.editedItem.name}`
+      } else {
+        this.close()
+      }
+    }
+  },
+
+  computed: {
+    ...mapState([
+      'admin',
+      'user'
+    ]),
+
+    dialog: {
+      get () {
+        return this.itemDialog
+      },
+
+      set (value) {
+        this.$emit('update:itemDialog', value)
+      }
+    },
+
+    currentItem: {
+      get () {
+        return this.selectedItem
+      },
+
+      set (value) {
+        this.$emit('update:selectedItem', value)
+      }
+    },
+
+    index: {
+      get () {
+        return this.editedIndex
+      },
+
+      set (value) {
+        this.$emit('update:editedIndex', value)
+      }
+    },
+
+    items: {
+      get () {
+        return this.itemList
+      },
+
+      set (value) {
+        this.$emit('update:itemList', value)
+      }
+    },
+
+    assays: {
+      get () {
+        return this.assayList
+      },
+
+      set (value) {
+        this.$emit('update:assayList', value)
+      }
+    },
+
+    vendors: {
+      get () {
+        return this.vendorList
+      },
+
+      set (value) {
+        this.$emit('update:vendorList', value)
+      }
+    }
+  },
+
+  methods: {
+    openSnack (text) {
+      this.snackText = text
+      this.snackbar = true
+    },
+
+    addAssay () {
+      this.assayForm = 'New Assay'
+      this.assayDialog = true
+      this.alert = false
+    },
+
+    addVendor () {
+      this.vendorForm = 'New Vendor'
+      this.vendorDialog = true
+      this.alert = false
+    },
+
+    close () {
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.index = -1
+        this.currentItem = {}
+        this.dialog = false
+      }, 300)
+    },
+
+    validateData () {
+      // checks input entry for numbers
+      if (this.$refs.form.validate()) {
+        this.alert = false
+        this.loading = true
+        this.save()
+      } else {
+        this.alert = true
+        this.alertMessage = 'Please fix errors'
+      }
+    },
+
+    async save () {
+      let assayInfo = this.assays.find(assay => assay.id === this.editedItem.AssayId)
+
+      this.editedItem.catalogNumber = this.editedItem.catalogNumber.toUpperCase()
+      // scaling to account for weird JS math
+      this.editedItem.currentStock = parseInt(this.editedItem.currentStock * 100 + 0.01) / 100
+
+      if (this.index > -1) {
+        // existing item
+        let focusedItem = this.items[this.index]
+        this.editedItem.updatedAt = Date.now()
+        this.response = await itemService.put(focusedItem.id, this.editedItem, assayInfo)
+
+        if (this.response.status === 200) {
+          Object.assign(focusedItem, this.response.data[0])
+          this.snackText = `${this.response.data[0].name} updated`
+        }
+      } else {
+        // new item
+        this.response = await itemService.post(this.editedItem, assayInfo)
+
+        if (this.response.status === 200) {
+          this.items.push(this.response.data)
+          // this.supplies.push(this.response.data)
+          this.snackText = `${this.response.data.name} saved`
+        }
+      }
+
+      if (!this.alert) {
+        this.loading = false
+        this.openSnack(this.snackText)
+        this.close()
+      }
     }
   }
 }
