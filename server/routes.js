bbe125f405716de04a461c6f1226c243dd16d06a
rukaroa <rukaroa@gmail.com>
2019-01-31 11:01:23 -0600

    added token authentication to some routes

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ const AssaysController = require('./controllers/AssaysController')
 const OrdersController = require('./controllers/OrdersController')
 const DepartmentController = require('./controllers/DepartmentController')
 const EntryController = require('./controllers/EntryController')
+const isAuthenticated = require('./policies/isAuthenticated')
 // define placement of authentication
 
 module.exports = (app) => {
@@ -13,29 +14,29 @@ module.exports = (app) => {
     AuthenticationControllerPolicy.register,
     AuthenticationController.register)
   app.post('/login', AuthenticationController.login)
-  app.put('/userupdate', AuthenticationController.update)
+  app.put('/userupdate', isAuthenticated, AuthenticationController.update)
 
   app.get('/department', DepartmentController.index)
   app.get('/department/:deptId', DepartmentController.show)
-  app.post('/department', DepartmentController.post)
-  app.put('/department/:deptId', DepartmentController.put)
+  app.post('/department', isAuthenticated, DepartmentController.post)
+  app.put('/department/:deptId', isAuthenticated, DepartmentController.put)
 
   app.get('/vendors', VendorsController.index)
   app.get('/vendors/:vendorId', VendorsController.show)
-  app.post('/vendors', VendorsController.post)
-  app.put('/vendors/:vendorId', VendorsController.put)
+  app.post('/vendors', isAuthenticated, VendorsController.post)
+  app.put('/vendors/:vendorId', isAuthenticated, VendorsController.put)
 
   app.get('/items', ItemsController.index)
   app.get('/items/list', ItemsController.show)
-  app.post('/items', ItemsController.post)
+  app.post('/items', isAuthenticated, ItemsController.post)
   app.put('/items/:itemId', ItemsController.put)
-  app.put('/deactivate-items', ItemsController.deactivate)
-  app.put('/reassign-items', ItemsController.reassign)
+  app.put('/deactivate-items', isAuthenticated, ItemsController.deactivate)
+  app.put('/reassign-items', isAuthenticated, ItemsController.reassign)
 
   app.get('/assays', AssaysController.index)
   app.get('/assays/:assayId', AssaysController.show)
-  app.post('/assays', AssaysController.post)
-  app.put('/assays/:assayId', AssaysController.put)
+  app.post('/assays', isAuthenticated, AssaysController.post)
+  app.put('/assays/:assayId', isAuthenticated, AssaysController.put)
 
   app.get('/orders', OrdersController.index)
   app.get('/orders/:orderId', OrdersController.show)
