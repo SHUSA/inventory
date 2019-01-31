7cfd7c2ea1c215907c0fdddf382d82ccad81177a
rukaroa <rukaroa@gmail.com>
2019-01-31 07:55:34 -0600

    fixed API Calls

--- src/services/AuthenticationService.js
+++ src/services/AuthenticationService.js
@@ -3,19 +3,19 @@ import checkResponse from './checkResponse'
 
 export default {
   register (credentials) {
-    return API().get('register', credentials).catch(err =>
+    return API().post('register', credentials).catch(err =>
       checkResponse(err)
     )
   },
 
   login (credentials) {
-    return API().get('login', credentials).catch(err =>
+    return API().post('login', credentials).catch(err =>
       checkResponse(err)
     )
   },
 
   userupdate (user) {
-    return API().post('userupdate', user).catch(err =>
+    return API().put('userupdate', user).catch(err =>
       checkResponse(err)
     )
   }
