640e04eea5c1dbc8f9a0125225bfcb702d298403
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-03-19 08:02:36 -0500

    revert post deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -19,8 +19,8 @@ module.exports = {
       path : '/var/www/srl_inventory',
       // 'post-deploy': 'git pull && pm2 list && pm2 expose',
       // 'post-deploy': 'git pull && pm2 delete 0 && pm2 delete 1 && pm2 start',
-      'post-deploy': 'git pull && node_modules/.bin/sequelize db:migrate:undo',
-      // 'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
+      // 'post-deploy': 'git pull && node_modules/.bin/sequelize db:migrate:undo',
+      'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.148.45',
