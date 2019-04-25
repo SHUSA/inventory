3369eb1d17e7e721e6b2c6cc36b543fc0ab11198
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-04-25 11:55:15 -0500

    revert post-deploy to pull

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,8 +20,8 @@ module.exports = {
       // 'post-deploy': 'git pull && pm2 list && pm2 expose',
       // 'post-deploy': 'git pull && pm2 delete 0 && pm2 delete 1 && pm2 start',
       // 'post-deploy': 'git pull && node_modules/.bin/sequelize db:migrate:undo',
-      'post-deploy': 'git clone https://github.com/SHUSA/srl_inventory.git && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
-      // 'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
+      // 'post-deploy': 'git clone https://github.com/SHUSA/srl_inventory.git && npm install && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
+      'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
       "env"  : {
         'NODE_ENV': "'development'",
         SRL_INVENTORY_HOST: '10.212.17.96',
