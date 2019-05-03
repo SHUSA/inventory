7dc67b57a042844cf264ee2e67ca0f9aeb1ae3bc
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-05-03 11:46:23 -0500

    replace envs with exported variables

--- server/config/config.js
+++ server/config/config.js
@@ -1,14 +1,13 @@
-console.log('check envs')
-console.log(process.env)
+const db = require('../../../.inventory/db')
 
 module.exports = {
   port: process.env.SERVER_PORT || 8081,
   host: process.env.HOST || 'localhost',
 
   db: {
-    database: process.env.SRL_DB_NAME || 'srl_inventory',
-    user: process.env.POSTGRES_DATABASE_USERNAME || 'postgres',
-    password: process.env.POSTGRES_DATABASE_PASSWORD || 'root',
+    database: db.db_name || 'srl_inventory',
+    user: db.db_user || 'postgres',
+    password: db.db_password || 'root',
     options: {
       dialect: process.env.DIALECT || 'postgres',
       host: process.env.SRL_INVENTORY_HOST || 'localhost',
