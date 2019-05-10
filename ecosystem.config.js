bf3250d0416b7988575e479a13393574fc2842c7
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-05-10 10:35:08 -0500

    change to previous ports

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,5 @@
-const host = '10.212.17.95'
-const dbHost = '10.212.17.96'
+const host = '10.212.148.45'
+const dbHost = '10.212.148.42'
 const user = 'deployer'
 const path = '/var/www/srl_inventory'
 const port = 4040
