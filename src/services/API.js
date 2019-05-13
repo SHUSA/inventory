13383bbd98d18620594a897ee9ee5ee60eb45ebb
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-05-13 11:30:40 -0500

    troubleshooting port and hosts

--- src/services/API.js
+++ src/services/API.js
@@ -5,6 +5,10 @@ import store from '@/store/store'
 const port = process.env.SERVER_PORT || 8081
 const host = 'http://' + (process.env.HOST || 'localhost')
 
+console.log('API')
+console.log(`host ${host}`)
+console.log(`port ${port}`)
+
 let baseURL = `${host}:${port}`
 
 export default () => {
