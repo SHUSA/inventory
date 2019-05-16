79ffd82d051e34a16b8227edbcb57a85bccd2002
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2019-05-16 09:30:36 -0500

    removed cors optiosn

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const config = require('./server/config/config')
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
-app.use(cors({
-  exposedHeaders: ['Content-Length']
-}))
+app.use(cors())
 require('./server/passport')
 require('./server/routes')(app)
 
