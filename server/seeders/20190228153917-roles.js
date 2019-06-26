2573c34c0d3bdeea00f827eab8f5c9bed1ac0d75
rukaroa <rukaroa@gmail.com>
2019-06-26 10:38:19 -0500

    fixed seed

--- server/seeders/20190228153917-roles.js
+++ server/seeders/20190228153917-roles.js
@@ -4,37 +4,28 @@ const { Role } = require('../models')
 
 module.exports = {
   up: async function (queryInterface, Sequelize) {
-    queryInterface.bulkInsert('Roles', [{
-      name: 'User'
-    }, {
-      name: 'Sub Admin',
-      isSubAdmin: true
-    }, {
-      name: 'Admin',
-      isAdmin: true
-    }, {
-      name: 'Super Admin',
-      isSuperAdmin: true
-    }])
-
-    queryInterface.bulkInsert('Departments', [{
+    await Role.create({ name: 'User' })
+    await Role.create({ name: 'Sub Admin', isSubAdmin: true })
+    await Role.create({ name: 'Admin', isAdmin: true })
+    await Role.create({ name: 'Super Admin', isSuperAdmin: true })
+    await Department.create({
       name: 'All',
       all: true,
       location: 'All'
-    }])
+    })
 
     const department = (await Department.findOne({ where: { all: true } })).toJSON()
     const role = (await Role.findOne({ where: { isSuperAdmin: true } })).toJSON()
 
     await User.create({
-      name: 'Administrator',
+      username: 'Administrator',
       RoleId: role.id
     }, department)
   },
 
-  down: function (queryInterface, Sequelize) {
-    queryInterface.bulkDelete('Users', null, {})
-    queryInterface.bulkDelete('Roles', null, {})
-    queryInterface.bulkDelete('Departments', null, {})
+  down: async function (queryInterface, Sequelize) {
+    await User.destroy({ where: { username: 'Administrator' } })
+    await Role.destroy({ where: {} })
+    await Department.destroy({ where: { name: 'All' } })
   }
 }
