288545eb73b46c6ca7b5ec04b66ce4e374eaefc4
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-17 09:22:52 -0600

    adding env settings

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,11 @@
 'use strict'
+const env = require('../ecosystem.config').devSettings
 console.log('production started')
 module.exports = {
   NODE_ENV: '"production"',
-  PORT: JSON.stringify(process.env.PORT),
-  SERVER_PORT: JSON.stringify(process.env.SERVER_PORT),
-  HOST: JSON.stringify(process.env.HOST),
+  PORT: JSON.stringify(process.env.PORT) || env.PORT,
+  SERVER_PORT: JSON.stringify(process.env.SERVER_PORT) || env.SERVER_PORT,
+  HOST: JSON.stringify(process.env.HOST) || env.HOST,
   devtools: false,
   debug: false,
   silent: true
