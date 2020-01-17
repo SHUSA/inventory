8d0eaf6f5fa6cea8da9574ba6a817cd60e474adb
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-17 09:41:00 -0600

    to do comment

--- src/services/API.js
+++ src/services/API.js
@@ -3,6 +3,7 @@ import store from '@/store/store'
 const env = require('../../ecosystem.config').devSettings
 
 // env taken from webpack
+// to do: investigate why envs are not passing normally
 const port = process.env.SERVER_PORT || env.SERVER_PORT || 8081
 const host = 'http://' + (process.env.HOST || env.HOST || 'localhost')
 
