f6ba767391f2712a847e834be2793bceab3c6f9b
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-17 09:12:20 -0600

    revert; not a webpack problem

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,11 +1,10 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-const env = require('../ecosystem.config').devSettings
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  PORT: JSON.stringify(process.env.PORT) || env.PORT,
-  SERVER_PORT: JSON.stringify(process.env.SERVER_PORT) || env.SERVER_PORT,
-  HOST: JSON.stringify(process.env.HOST) || env.HOST,
+  PORT: JSON.stringify(process.env.PORT),
+  SERVER_PORT: JSON.stringify(process.env.SERVER_PORT),
+  HOST: JSON.stringify(process.env.HOST),
 })
