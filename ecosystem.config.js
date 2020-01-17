cde2aa57abcb8161e186a7afede783c6873086c3
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-17 06:12:20 -0600

    made settings const

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,21 @@ const user = 'deployer'
 const path = '/var/www/srl_inventory'
 const port = 80
 const serverPort = 3000
+const devSettings = {
+  NODE_ENV: 'development',
+  SRL_INVENTORY_HOST: dbHost,
+  SRL_INVENTORY_DB_PORT: 5432,
+  PORT: port,
+  SERVER_PORT: serverPort,
+  HOST: host,
+  NEWDB: 'false'
+}
 
 module.exports = {
   apps : [{
     name      : 'srl_inventory',
     script    : 'server.js',
-    env: { 
-      NODE_ENV: 'development'
-    },
+    env: devSettings,
     env_production : {
       NODE_ENV: 'production'
     }
@@ -26,16 +33,8 @@ module.exports = {
       path : path,
       // 'post-deploy': 'git pull && node_modules/.bin/sequelize db:migrate:undo',
       'post-deploy': 'git pull && npm install && node_modules/.bin/sequelize db:migrate && npm run build && pm2 reload ecosystem.config.js --env development --update-env && pm2 list && pm2 logs',
-      "env"  : {
-        'NODE_ENV': 'development',
-        SRL_INVENTORY_HOST: dbHost,
-        SRL_INVENTORY_DB_PORT: 5432,
-        PORT: port,
-        SERVER_PORT: serverPort,
-        HOST: host,
-        NEWDB: 'false'
-      },
-      "post-setup": `HOST=${host} PORT=${port} SRL_INVENTORY_HOST=${dbHost} npm install && npm start`
+      env  : devSettings,
+      'post-setup': `HOST=${host} PORT=${port} SRL_INVENTORY_HOST=${dbHost} npm install && npm start`
     },
     production : {
       user : user,
@@ -44,8 +43,8 @@ module.exports = {
       repo : 'git@github.com:SHUSA/srl_inventory.git',
       path : path,
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      "env"  : {
-        "NODE_ENV": "production"
+      'env'  : {
+        NODE_ENV: 'production'
       }
     }
   }
