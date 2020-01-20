8dfe6a4a5bd090bd3a41d9f7c236dae16b81e0da
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-20 05:32:47 -0600

    testing restart sequence

--- src/services/API.js
+++ src/services/API.js
@@ -4,8 +4,8 @@ const env = require('../../ecosystem.config').devSettings
 
 // env taken from webpack
 // to do: investigate why envs are not passing normally
-const port = process.env.SERVER_PORT || env.SERVER_PORT || 8081
-const host = 'http://' + (process.env.HOST || env.HOST || 'localhost')
+const port = process.env.SERVER_PORT || 8081 // || env.SERVER_PORT
+const host = 'http://' + (process.env.HOST || 'localhost' || env.HOST)
 
 const baseURL = `${host}:${port}`
 
