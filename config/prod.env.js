8dc9c4b2b2d9cc44c4602d6f4cd8e8114d35c9fe
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-20 11:45:56 -0600

    make loggin readable

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,8 @@
 'use strict'
 console.log('production started')
-console.log(JSON.stringify(process.env))
+console.log(JSON.stringify(process.env.PORT))
+console.log(JSON.stringify(process.env.SERVER_PORT))
+console.log(JSON.stringify(process.env.HOST))
 const settings = {
   NODE_ENV: '"production"',
   PORT: JSON.stringify(process.env.PORT),
