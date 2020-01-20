86d37a2bda6930d027cd28f40f57b855da67fc63
Paul E. Dimaano <srlped@US.Int.SonicHealthcare>
2020-01-20 11:52:24 -0600

    remove dev import

--- src/services/API.js
+++ src/services/API.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
 import store from '@/store/store'
-const env = require('../../ecosystem.config').devSettings
 
 // env taken from webpack
 // to do: investigate why envs are not passing normally
-const port = process.env.SERVER_PORT || env.SERVER_PORT || 8081
-const host = 'http://' + (process.env.HOST || env.HOST || 'localhost')
+const port = process.env.SERVER_PORT || 8081
+const host = 'http://' + (process.env.HOST || 'localhost')
 
 const baseURL = `${host}:${port}`
 
